d8f6fa21194c7222131f763d86745e5ab3a2809b
Ollie Bennett <olliebennett@gmail.com>
2014-05-29 23:06:13 +0100

    Add Vagrantfile and provisioning script for Rails

---
+++ bootstrap.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Update
+apt-get update
+
+# Git
+apt-get install -y git
+
+# Install rbenv (Ruby Environment)
+# git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+# echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+# source ~/.bashrc
+
+# Install correct version of Ruby
+apt-get install -y ruby1.9.3
+
+#update-alternatives --set ruby /usr/bin/ruby1.9.1
+
+#gem install bundler
+
+# Install Rails dependencies
+# ... avoid 'make: command not found' error.
+apt-get install -y build-essential
+# ... avoid issues when running 'bundle install'
+apt-get install -y libxslt-dev libxml2-dev libsqlite3-dev
+
+# ... avoid issues with Error installing pg: ERROR: Failed to build gem native extension.
+apt-get install -y libpq-dev
+
+# Install Ruby on Rails!
+gem install rails
+
+# Install ExecJS (required to find JavaScript runtime)
+gem install execjs
+
+# NodeJS (JavaScript runtime)
+apt-get install -y nodejs
+
+# Move to the /vagrant (shared) directory.
+cd /vagrant
+
+# Use bundler to manage (Ruby) dependencies
+bundle install
+
+# Run any new db migrations
+bundle exec rake db:migrate
