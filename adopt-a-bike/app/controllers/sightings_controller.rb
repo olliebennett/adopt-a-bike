e9a60f004baa97cf2aecb87b2aa5a6e2764ad323
Ollie Bennett <olliebennett@gmail.com>
2014-05-31 00:02:06 +0100

    Add photos to sightings

--- adopt-a-bike/app/controllers/sightings_controller.rb
+++ adopt-a-bike/app/controllers/sightings_controller.rb
@@ -69,6 +69,6 @@ class SightingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sighting_params
-      params.require(:sighting).permit(:bike_id, :lat, :long, :time)
+      params.require(:sighting).permit(:bike_id, :lat, :long, :time, :photo)
     end
 end
--- adopt-a-bike/app/models/sighting.rb
+++ adopt-a-bike/app/models/sighting.rb
@@ -1,3 +1,11 @@
 class Sighting < ActiveRecord::Base
+  
+  # A sighting is of a specific bike.
   belongs_to :bike
+
+  # A sighting has an associated photo
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/default.png"
+  # validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  do_not_validate_attachment_file_type :photo
+
 end
