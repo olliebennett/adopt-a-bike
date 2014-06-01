a49ed953e3b943eb991361c49c8a6aeb0bfd984c
Ollie Bennett <olliebennett@gmail.com>
2014-06-01 16:35:52 +0100

    Configure mailcatcher (with README info)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
   # Default URL Options
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
 end
