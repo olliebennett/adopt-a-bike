9a58284bb62e8d53e3e53056efd0bd281c48e35a
Ollie Bennett <olliebennett@gmail.com>
2019-01-22 13:38:40 +0000

    Ensure bikes have a number

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,8 +3,9 @@ class Bike < ActiveRecord::Base
   # A bike can be sighted many times!
   has_many :sightings, dependent: :destroy
 
+  validates :number, presence: true
+
   def to_param
     "#{number}"
   end
-
 end
