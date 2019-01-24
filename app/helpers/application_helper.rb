241ca0928cd5d8d9eb7af2475dd3e8bdfc8441de
Ollie Bennett <olliebennett@gmail.com>
2019-01-24 22:26:06 +0000

    Display map for station/sighting locations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def map_url(obj)
+    "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/static/#{obj.long},#{obj.lat},14,0,0/300x200?access_token=#{Rails.application.credentials.mapbox[:access_token]}"
+  end
 end
